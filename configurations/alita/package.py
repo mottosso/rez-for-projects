5a8da39725d1f8ecb78b2b0878f012fe2d502856
Marcus Ottosson <konstruktion@gmail.com>
2019-04-22 09:55:43 +0100

    Cosmetics

--- configurations/alita/package.py
+++ configurations/alita/package.py
@@ -4,66 +4,67 @@ name = "alita"
 version = "0.3.17"
 
 build_command = "python -m rezutils {root}"
+private_build_requires = ["rezutils-1"]
 
-private_build_requires = [
-    "rezutils-1",
-]
+_requires = {
+    "": [
+        "base-1",
+        "python-2.7",
 
-build_requires = [
-    "base-1",
-    "python-2.7",
+        # Supported DCCs, if either of these are used,
+        # this must be their version.
+        "~maya-2018",
+        "~nuke-11",
+        "~houdinifx-17",
+        "~aftereffects-cs6",
+        "~photoshop-2018",
+    ],
 
-    # Supported DCCs, if either of these are used,
-    # this must be their version.
-    "~maya-2018",
-    "~nuke-11",
-    "~houdinifx-17",
-    "~aftereffects-cs6",
-    "~photoshop-2018",
-]
-
-# Shared requirements, used by all DCCs
-dcc_requires = [
-    "pyblish_base-1.4",
-]
-
-# DCC-specific requirements
-maya_requires = dcc_requires + [
-    "mgear-2.4",
-]
-
-nuke_requires = dcc_requires + []
-
-environ = {
-    "PROJECT_NAME": "Alita",
-    "PROJECT_PATH": "{env.PROJECTS_PATH}/alita",
-
-    # For locating in e.g. ftrack
-    "PRODUCTION_TRACKER_ID": "alita-123",
+    # Requirements relative a request
+    # E.g. if `alita maya` is requested, the "maya"
+    # requirements are added to the list.
+    "maya": [
+        "pyblish_base-1.4",
+        "mgear-2.4",
+    ],
+    "nuke": [
+        "pyblish_base-1.4",
+    ]
 }
 
-maya_environ = {
-    "MAYA_COLOR_MANAGEMENT_POLICY_FILE": [
-        "{root}/maya/color_management/default_synColorConfig.xml"
-    ],
+_environ = {
+    "": {
+        "PROJECT_NAME": "Alita",
+        "PROJECT_PATH": "{env.PROJECTS_PATH}/alita",
 
-    "PYTHONPATH": [
-        "{root}/maya/scripts",
-        "{root}/maya/shelves",
-    ],
+        # For locating in e.g. ftrack
+        "PRODUCTION_TRACKER_ID": "alita-123",
+    },
 
-    "MAYA_PLUG_IN_PATH": [
-        "{root}/maya/plugins"
-    ],
+    # Environment relative a request
+    "maya": {
+        "MAYA_COLOR_MANAGEMENT_POLICY_FILE": [
+            "{root}/maya/color_management/default_synColorConfig.xml"
+        ],
 
-    "MAYA_SCRIPT_PATH": [
-        "{root}/maya/scripts",
-    ],
+        "PYTHONPATH": [
+            "{root}/maya/scripts",
+            "{root}/maya/shelves",
+        ],
 
-    "MAYA_SHELF_PATH": "{root}/maya/shelves",
-    "XBMLANGPATH": [
-        "{root}/maya/shelves/icons"
-    ],
+        "MAYA_PLUG_IN_PATH": [
+            "{root}/maya/plugins"
+        ],
+
+        "MAYA_SCRIPT_PATH": [
+            "{root}/maya/scripts",
+        ],
+
+        "MAYA_SHELF_PATH": "{root}/maya/shelves",
+        "XBMLANGPATH": [
+            "{root}/maya/shelves/icons"
+        ],
+    }
 }
 
 
@@ -72,13 +73,14 @@ def requires():
     global this
     global request
     global in_context
-    requires = this.build_requires[:]
 
-    if in_context() and "maya" in request:
-        requires += this.maya_requires
+    _requires = this._requires
+    requires = _requires[""][:]
 
-    if in_context() and "nuke" in request:
-        requires += this.nuke_requires
+    # Add DCC-specific requirements
+    if in_context():
+        for dcc in request:
+            requires += _requires.get(dcc, [])
 
     return requires
 
@@ -89,12 +91,12 @@ def commands():
     global request
     global expandvars
 
-    environ = this.environ
+    _environ = this._environ
 
-    if "maya" in request:
-        environ.update(this.maya_environ)
+    for name in request:
+        _environ.update(_environ.get(name, {}))
 
-    for key, value in this.environ.items():
+    for key, value in _environ.items():
         if isinstance(value, (tuple, list)):
             [env[key].append(expandvars(v)) for v in value]
         else:
